41aa44c02603a41c15b4c0110fe97f596db3e638
neha <neha.thiyagarajan@linqia.com>
2017-10-16 16:44:15 -0700

    saving

--- finding_phone_numbers.js
+++ finding_phone_numbers.js
@@ -1,22 +1,20 @@
-
-function getPhoneNumbers(string) {
-    if(string.length < 2) {
-        return string;
-    }
-    var phoneNumbers = [];
-    for (var i=0; i< string.length; i++) {
-        var char = string[i];
-        if (string.indexOf(char) != i) // if char was used already
-            continue;           // skip it this time
-
-        var remainingString = string.slice(0,i) + string.slice(i+1,string.length);
-        for (var subPermutation of getPhoneNumbers(remainingString)){
-            var temp = char + subPermutation;
-            phoneNumbers.push(temp)
+function getPhoneNumbers(number, firstFew) {
+    if (number.length == 0) {
+        console.log(firstFew);
+    } else {
+        for (var i = 0; i < number.length; i++) {
+            var currentLetter = number.substring(i, i + 1);
+            var rest = number.replace(currentLetter,"");
+            var newfirstFew = firstFew.concat(currentLetter);
+            getPhoneNumbers(rest,newfirstFew);
         }
-
     }
-    return phoneNumbers;
 }
 
-console.log(getPhoneNumbers('0123456789'));
+function getPhoneNumbersInSameArea(number){
+    // will make the first three digits as constant and give permutations of the other 7 digits
+    var area = number.substring(0,3);
+    var phoneNumber = number.replace(area,"");
+    getPhoneNumbers(phoneNumber,area);
+}
+getPhoneNumbersInSameArea('1234567890');
